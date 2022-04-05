c6e2f85c9043482dd2a362d43065296c8ea4e338
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-05 13:10:17 -0400

    commit tree ae3d0f438f2f5ea78f0e20b4747fee3dca367f43

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves dwarves 
+  dwarves.map.with_index(1) do |dwarf, i|
+    puts "#{i}. #{dwarf}"
+    
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet calls
+  calls.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
